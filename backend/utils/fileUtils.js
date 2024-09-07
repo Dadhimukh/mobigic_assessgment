956af115f64da4c65ea8fc9bc88986fb922b90b3
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2024-09-08 00:08:05 +0530

    Removed files data and generate 6 digit code function from backend

--- backend/utils/fileUtils.js
+++
@@ -1,8 +0,0 @@
-// backend/utils/fileUtils.js
-
-// Function to generate a unique 6-digit code
-const generateUniqueCode = () => {
-    return Math.floor(100000 + Math.random() * 900000).toString(); // Generates a 6-digit number
-};
-
-module.exports = { generateUniqueCode };
